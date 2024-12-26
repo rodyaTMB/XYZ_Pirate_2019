ad9c63fc193537b927e7a22b5a26bd665f6c3454
Rodya <rodyatmb@gmail.com>
2024-12-27 00:38:18 +0300

    Legacy move
    Разобрали работу со старенькой инпут систем, но которая до сих пор встречается в ассетах и разных туторах

---
+++ XYZ_Pirate_2019/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+	[SerializeField] private HeroPirate _heroPirate;
+
+	void Awake()
+	{
+		//_heroPirate = GetComponent<HeroPirate>();
+	}
+
+	void Update()
+	{
+		LegacyMove();
+	}
+
+	public void LegacyMove()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		_heroPirate.SetDirectionX(horizontal);
+
+		var vertical = Input.GetAxis("Vertical");
+		_heroPirate.SetDirectionY(vertical);
+	}
+
+	public void Move()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			_heroPirate.SetDirectionX(-1f);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			_heroPirate.SetDirectionX(1f);
+		}
+		else
+		{
+			_heroPirate.SetDirectionX(0);
+		}
+	}
+}
---
+++ XYZ_Pirate_2019/Assets/Scripts/HeroPirate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPirate : MonoBehaviour
+{
+
+	[SerializeField] float _speed;
+	[SerializeField] float _jumpSpeed;
+
+	float _directionX;
+	float _directionY;
+
+	public void SetDirectionX(float direction)
+	{
+		_directionX = direction;
+	}
+
+	public void SetDirectionY(float direction)
+	{
+		_directionY = direction;
+	}
+
+	void Update()
+	{
+		if (_directionX != 0 || _directionY != 0)
+		{
+			
+			var deltaX = _directionX * _speed * Time.deltaTime;
+			var deltaY = _directionY * _jumpSpeed * Time.deltaTime;
+			
+			var newXPosition = transform.position.x + deltaX;
+			var newYPosition = transform.position.y + deltaY;
+			
+			transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
+		}		
+	}
+}
